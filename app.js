f4b52a967be28197077caac85a002533ac4728e2
TRAN_QUANG_DO <do.tranquang@neo-career.co.jp>
2021-04-17 17:39:07 +0900

    20210417 17:38

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 new Vue({
-  el: '#tasks',
-  data:{
-     tasks: [
-			{body:"Học NodeJS", completed: false},
-			{body:"Học VueJS", completed: true},
-			],
-	 newTask: ''
-  },
-  computed: {
-	dadones: function(){
-		return this.tasks.filter(function(task){
-			return task.completed;
-		})
-	},
-	chuadones: function(){
-		return this.tasks.filter(function(task){
-			return ! task.completed;
-		})
-	}
-  },
-  methods:{
-     addTask: function(e){
-        e.preventDefault();
-		if(!this.newTask) return;
-        this.tasks.push({
-            body: this.newTask,
+   el: '#tasks',
+   data: {
+      vueTasks: [
+         { body: "Học NodeJS", completed: false },
+         { body: "Học VueJS", completed: true },
+      ],
+      vueNewTask: ''
+   },
+   computed: {
+      onCompDone: function () {
+         return this.vueTasks.filter(function (task) {
+            return task.completed;
+         })
+      },
+      onCompNotYet: function () {
+         return this.vueTasks.filter(function (task) {
+            return !task.completed;
+         })
+      }
+   },
+   methods: {
+      onAddTask: function (e) {
+         e.preventDefault();
+         if (!this.vueNewTask) return;
+         this.vueTasks.push({
+            body: this.vueNewTask,
             completed: false
-        });
-        this.newTask = '';
-     },
-     removeTask: function(task){
-        this.tasks.splice(this.tasks.indexOf(task), 1);
-     },
-     hoanthanhTask: function(task){
-        task.completed = true;
-     },
-     change2NotDoneTask: function(task){
-        task.completed = false;
-     },
-     hoanthanhAll: function(){
-		this.tasks.forEach(function(task){
-			task.completed = true;
-		})
-     },
-     removeAllDone: function(){
-		this.tasks = this.tasks.filter(function(task){
-			return ! task.completed;
-		})
-     },
-     editTask: function(task){
-		this.removeTask(task);
-		this.newTask = task.body;
-		this.$refs.taskmoi.focus();
-     }
-  }
+         });
+         this.vueNewTask = '';
+      },
+      onRemoveTask: function (task) {
+         this.vueTasks.splice(this.vueTasks.indexOf(task), 1);
+      },
+      onHoanThanhTask: function (task) {
+         task.completed = true;
+      },
+      onChange2NotDoneTask: function (task) {
+         task.completed = false;
+      },
+      onHoanThanhAll: function () {
+         this.vueTasks.forEach(function (task) {
+            task.completed = true;
+         })
+      },
+      onRemoveAllDone: function () {
+         this.vueTasks = this.vueTasks.filter(function (task) {
+            return !task.completed;
+         })
+      },
+      onEditTask: function (task) {
+         this.onRemoveTask(task);
+         this.vueNewTask = task.body;
+         this.$refs.taskmoi.focus();
+      }
+   }
 })
